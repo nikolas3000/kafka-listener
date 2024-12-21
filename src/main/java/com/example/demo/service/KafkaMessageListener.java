883dc7947c6134f7df5145c8a7034db310d6b14d
nikolas3000 <kolya.eremenko93@gmail.com>
2024-12-21 14:33:17 +0300

    add just read msg

--- src/main/java/com/example/demo/service/KafkaMessageListener.java
+++ src/main/java/com/example/demo/service/KafkaMessageListener.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.example.demo.model.MessageRecord;
 import com.example.demo.repositories.MessageRecordRepository;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -12,10 +13,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaMessageListener {
+    private static final Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
     private final MessageRecordRepository messageRecordRepository;
 
     public KafkaMessageListener(MessageRecordRepository messageRecordRepository) {
         this.messageRecordRepository = messageRecordRepository;
+    }
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void justRead (String message){
+        System.out.println("Получено сообщение: " +message);
+        log.info("Получено сообщение: {}", message);
     }
 
     @KafkaListener(topics = "kafka-topic-test", groupId = "my-group")
@@ -34,9 +41,9 @@ public class KafkaMessageListener {
             messageRecord.setTimeRq(timestamp);
             messageRecordRepository.save(messageRecord);
 
-            System.out.println("Записано в БД: msgId=" + msgId + ", timeRq=" + timestamp);
+            log.info("Записано в БД: msgId={}, timeRq={}", msgId, timestamp);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Ошибка при обработке сообщения: {}", e.getMessage(), e);
         }
     }
 }
